fb25e97d02acd3073adbae1c0825f82ecde0ebab
Mario Roberti <mario.roberti@gmail.com>
2023-05-20 15:50:02 -0700

    Re re created sword ability and dropping instance
    Goddamned fucking git and stupid ass VC

--- scenes/ability/sword_ability_controller/sword_ability_controller.gd
+++ scenes/ability/sword_ability_controller/sword_ability_controller.gd
@@ -4,9 +4,13 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$Timer.timeout.connect(on_timer_timeout)
 
+func on_timer_timeout():
+	var player = get_tree().get_first_node_in_group("player") as Node2D
+	if player == null:
+		return
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	var sword_instance = sword_ability.instantiate() as Node2D
+	player.get_parent().add_child(sword_instance)
+	sword_instance.global_position = player.global_position
