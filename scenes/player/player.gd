ec0cd992ffaec1102964a7360eb3468b302b36ff
Mario Roberti <mario.roberti@gmail.com>
2023-05-23 08:52:27 -0700

    Added EnemySpawner
    Added a spawner object

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -15,9 +15,7 @@ func _process(delta):
 	move_and_slide()
 
 
-
 func get_movement_vector():
-	var movement_vector = Vector2.ZERO
 	var x_movement = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	var y_movement = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	return Vector2(x_movement,y_movement)
